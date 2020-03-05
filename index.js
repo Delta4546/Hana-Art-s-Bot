bd48e36ff4b126e90470dd1935a1aa5923fcfb9e
ShineLoliMC <thomasbar59@gmail.com>
2020-03-05 18:28:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const HashMap = require("hashmap")
+
+const playersingame = new HashMap();
+const numberplayer = new HashMap();
+
+//NOT ME
+const fs = require('fs');
+bot.cmds = new Discord.Collection();
+
+const cmdsf = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of cmdsf) {
+    const cmd = require(`./commands/${file}`);
+    bot.cmds.set(cmd.name, cmd);
+    console.log(cmd.name+" was loaded !")
+}
+
+//NOT ME
+
+
+bot.on("ready", () => {
+    bot.user.setActivity("Hana Art's | h.help");
+})
+
+bot.on('message', async msg => {
+    if(msg.author.bot) return;
+    if(!msg.content.startsWith("h.")) return;
+    let args = msg.content.replace("h.", "").split(" ");
+    const cmd = bot.cmds.get(args[0]);
+    try{
+        if(cmd.name == "play"){
+            cmd.execute(msg, args, playersingame, numberplayer);
+            return;
+        }
+        cmd.execute(msg, args);
+    }catch (error){
+        return;
+    }
+});
+
+bot.login([YOUR TOKEN]);
